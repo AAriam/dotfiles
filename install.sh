efb5e63029ebbe39dbaa6a2b36cfcf96b6f33b8f
AAriam <80158628+AAriam@users.noreply.github.com>
2025-03-27 18:14:26 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# DotFiles Repository Installation Script
+# =======================================
+# This script installs the dotfiles repository using chezmoi.
+#
+# References
+# ----------
+# - https://github.com/twpayne/dotfiles/blob/master/install.sh
+
+set -e # -e: exit on error
+
+if [ ! "$(command -v chezmoi)" ]; then
+  # Install chezmoi
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  # Already installed
+  chezmoi=chezmoi
+fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
