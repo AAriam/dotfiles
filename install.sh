2820fbb9756cb3222a22ec729dffb3847b1fe583
AAriam <80158628+AAriam@users.noreply.github.com>
2025-03-28 15:33:36 +0100

    add comments

--- install.sh
+++ install.sh
@@ -2,18 +2,24 @@
 
 # DotFiles Repository Installation Script
 # =======================================
-# This script installs the dotfiles repository using chezmoi.
+# This script installs chezmoi if not already installed,
+# and uses it to install the dotfiles repository.
 #
 # References
 # ----------
 # - https://github.com/twpayne/dotfiles/blob/master/install.sh
 
-set -e # -e: exit on error
+
+set -euo pipefail
 
 if [ ! "$(command -v chezmoi)" ]; then
   # Install chezmoi
+  
+  # By default, chezmoi is installed to `./bin`.
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
+  # Installation location can be specified with the `-b` flag.
+  # - https://www.chezmoi.io/install/#one-line-binary-install
   if [ "$(command -v curl)" ]; then
     sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
   elif [ "$(command -v wget)" ]; then
@@ -27,7 +33,9 @@ else
   chezmoi=chezmoi
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+# Get the local path to this dotfiles repository
+# by finding this script's directory.
+# - https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
+# Use `exec` to replace current process with `chezmoi init`
 exec "$chezmoi" init --apply "--source=$script_dir"
